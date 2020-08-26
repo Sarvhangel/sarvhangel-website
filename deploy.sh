31cd8c1a2946316ae330353df632394269ed52f6
Sarvhangel <sarvhangel@gmail.com>
2020-08-26 22:12:50 +0200

    [ADD] Update repo name

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ cat /dev/null > .gitmodules
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Adding git submodules
-git submodule add --force -b master https://github.com/Sarvhangel/sarvhangel.github.io public
+git submodule add --force -b master https://github.com/Sarvhangel/Sarvhangel.github.io public
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
